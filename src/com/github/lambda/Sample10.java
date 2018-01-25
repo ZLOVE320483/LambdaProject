8aa6f6038a01c6c4dac13ec9191c82071c367fcc
zlove <zlove.zhang@musical.ly>
2018-01-25 21:52:30 +0800

    Add Lambda Sample11 for IntSummaryStatistics

--- src/com/github/lambda/Sample10.java
+++ src/com/github/lambda/Sample10.java
@@ -1,7 +1,17 @@
 package com.github.lambda;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by zlove on 2018/1/25.
  */
 public class Sample10 {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
+        List<Integer> distinct = numbers.stream().map(i->i*i).distinct().collect(Collectors.toList());
+        System.out.printf("Original List : %s,  Square Without duplicates : %s %n", numbers, distinct);
+    }
 }
---
+++ src/com/github/lambda/Sample11.java
@@ -0,0 +1,20 @@
+package com.github.lambda;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+
+/**
+ * Created by zlove on 2018/1/25.
+ */
+public class Sample11 {
+
+    public static void main(String[] args) {
+        List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+        IntSummaryStatistics stats = primes.stream().mapToInt(x->x).summaryStatistics();
+        System.out.println("Highest prime number in List : " + stats.getMax());
+        System.out.println("Lowest prime number in List : " + stats.getMin());
+        System.out.println("Sum of all prime numbers : " + stats.getSum());
+        System.out.println("Average of all prime numbers : " + stats.getAverage());
+    }
+}
